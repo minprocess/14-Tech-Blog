b869eb64cd9da7aacd5e684064a50523dee3b11e
Bill Pate <wtpate69@hotmail.com>
2021-06-17 19:31:41 -0400

    set sequelize parameters separately in connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,26 @@ if (process.env.JAWSDB_URL) {
   console.log("connection.js jawsdb")
   console.log(process.env.JAWSDB_URL)
   console.log("\n\n");
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+//  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(
+    'q8d1de789dtvy7lq',
+    'dgb7xflqhb3s0vzp',
+    'dmhg77tj26k52y1j',
+    {
+      host: 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+
+/*
+Host	y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	
+Username	dgb7xflqhb3s0vzp
+Password	dmhg77tj26k52y1j
+Port	3306
+Database	q8d1de789dtvy7lq
+*/
+
 } else {
   console.log("new sequelize on local pc")
   sequelize = new Sequelize(
