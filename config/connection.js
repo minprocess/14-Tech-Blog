83a2c83312e6f6f94ec709611aa69edcf3d55c53
Bill Pate <wtpate69@hotmail.com>
2021-07-09 20:15:58 -0400

    fix connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,44 +1,24 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+console.log("name ",process.env.DB_NAME, ",  user ",
+  process.env.DB_USER, ",  pwd  ",
+  process.env.DB_PASSWORD,)
 
-if (process.env.JAWSDB_URL) {
-  console.log("connection.js jawsdb")
-  console.log(process.env.JAWSDB_URL)
-  console.log("\n\n");
-//  sequelize = new Sequelize(process.env.JAWSDB_URL);
-  sequelize = new Sequelize(
-    'q8d1de789dtvy7lq',
-    'dgb7xflqhb3s0vzp',
-    'dmhg77tj26k52y1j',
-    {
-      host: 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+  let sequelize;
+  if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+      },
+    );
+  }
 
-/*
-Host	y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	
-Username	dgb7xflqhb3s0vzp
-Password	dmhg77tj26k52y1j
-Port	3306
-Database	q8d1de789dtvy7lq
-*/
-
-} else {
-  console.log("new sequelize on local pc")
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
-
-module.exports = sequelize;
+  module.exports = sequelize;
